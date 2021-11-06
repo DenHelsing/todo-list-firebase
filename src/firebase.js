3008175c70d770530eef1c1a0cc3960ccf3dc218
DenHelsing <denisanatoliyovich@gmail,com>
2021-11-07 00:49:40 +0200

    some changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
-import firebase from 'firebase/app';
-import 'firebase/database';
+// import firebase from 'firebase';
+// // import 'firebase/database';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAetX5KzAxa8ix7wEWJVEGDcebVBjSHUJI',
-  authDomain: 'tododododododo.firebaseapp.com',
-  databaseURL:
-    'https://tododododododo-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'tododododododo',
-  storageBucket: 'tododododododo.appspot.com',
-  messagingSenderId: '186089720418',
-  appId: '1:186089720418:web:033f53d052f12d4d4b9f3b',
-  measurementId: 'G-WGHP7WS7T8'
-};
+// // const firebaseConfig = ;
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp({
+//   apiKey: 'AIzaSyAetX5KzAxa8ix7wEWJVEGDcebVBjSHUJI',
+//   authDomain: 'tododododododo.firebaseapp.com',
+//   databaseURL:
+//     'https://tododododododo-default-rtdb.europe-west1.firebasedatabase.app',
+//   projectId: 'tododododododo',
+//   storageBucket: 'tododododododo.appspot.com',
+//   messagingSenderId: '186089720418',
+//   appId: '1:186089720418:web:033f53d052f12d4d4b9f3b',
+//   measurementId: 'G-WGHP7WS7T8'
+// });
 
-const databaseRef = firebase.database().ref();
+// const databaseRef = firebase.database().ref();
 
-export const todoListRef = databaseRef.child('todolist');
-export default firebase;
+// export const todoListRef = databaseRef.child('todolist');
+// export default firebase;
